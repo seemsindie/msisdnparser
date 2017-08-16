d762a5fa26a15bf61716dfa5814a9f09f0a56ec6
Ivan <seemsindie@gmail.com>
2017-08-16 16:22:20 +0200

    Msisnd parser and rpc server implemented

--- sources/Parser.php
+++ sources/Parser.php
@@ -1,15 +1,48 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: timelord
- * Date: 8/16/17
- * Time: 2:01 PM
- */
 
-namespace MsisdnParser;
+namespace Msisdn;
 
+
+use libphonenumber\NumberParseException;
+use libphonenumber\PhoneNumberToCarrierMapper;
+use libphonenumber\PhoneNumberUtil;
 
 class Parser
 {
+    public $mno = null;
+    public $country = null;
+    public $countryDilingCode = null;
+    public $number = null;
+    public $isValid = false;
 
+    protected $phoneNumberUtil = null;
+    protected $carrierMapperUtil = null;
+
+    public function __construct()
+    {
+        $this->phoneNumberUtil = PhoneNumberUtil::getInstance();
+        $this->carrierMapperUtil = PhoneNumberToCarrierMapper::getInstance();
+    }
+
+    public function parse($number)
+    {
+        try {
+            $phoneNumber = $this->phoneNumberUtil->parse($number, null);
+        } catch (NumberParseException $e) {
+            $this->isValid = false;
+            return $this;
+        }
+
+        $this->isValid = $this->phoneNumberUtil->isValidNumber($phoneNumber);
+
+        if($this->isValid)
+        {
+            $this->number = $phoneNumber->getNationalNumber();
+            $this->country = $this->phoneNumberUtil->getRegionCodeForNumber($phoneNumber);
+            $this->countryDilingCode = (int)$phoneNumber->getCountryCode();
+            $this->mno = $this->carrierMapperUtil->getNameForNumber($phoneNumber, 'en_US');
+        }
+
+        return $this;
+    }
 }
--- sources/index.php
+++ sources/index.php
@@ -1,3 +1,14 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
+
+use JsonRPC\Server;
+
+$parser = new \Msisdn\Parser();
+
+$server = new Server();
+
+$procedureHandler = $server->getProcedureHandler();
+$procedureHandler->withObject($parser);
+
+echo $server->execute();
